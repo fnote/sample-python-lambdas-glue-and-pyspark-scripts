a48f8ea454d250e757b0b93f07b9c4f674211094
sithija <sithija.thewahettige@sysco.com>
2021-05-04 15:50:43 +0530

    [TRSP-79] lint issues fixed price zone decompression job

--- src/price_zone/decompress_job.py
+++ src/price_zone/decompress_job.py
@@ -1,3 +1,8 @@
+"""
+Price zone etl decompression job
+"""
+# pylint: disable=import-error,too-few-public-methods
+
 import sys
 import time
 import smart_open
@@ -6,12 +11,17 @@ from awsglue.utils import getResolvedOptions
 
 
 class Configuration:
+    """
+    configurations class
+    """
     TRANSPORT_PARAMS = {"min_part_size": 200 * 1024 * 1024}
+
 
 args = getResolvedOptions(sys.argv, ['s3_path', 'decompressed_file_path'])
 inputFilePath = args['s3_path']
 outputFilePath = args['decompressed_file_path']
-print("Starting decompression of file %s at time %.7f to bucket %s" % (inputFilePath, time.time(), outputFilePath))
+print("Starting decompression of file %s at time %.7f to bucket %s" %
+      (inputFilePath, time.time(), outputFilePath))
 with smart_open.open(outputFilePath, 'w', transport_params=Configuration.TRANSPORT_PARAMS) as fout:
     for line in smart_open.open(inputFilePath, 'rb', encoding='utf8'):
         fout.write(line)
