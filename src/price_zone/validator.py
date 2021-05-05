8bfae43e812c5894e09e84c83383a96c42371653
sithija <sithija.thewahettige@sysco.com>
2021-05-05 12:23:46 +0530

    [TRSP-79] validator

--- src/price_zone/validator.py
+++ src/price_zone/validator.py
@@ -1,6 +1,14 @@
-from pyspark.sql.functions import isnan, length, to_timestamp
+"""
+validator to validate columns in incoming files
+"""
+# pylint: disable=line-too-long
+
+from pyspark.sql.functions import isnan, length
 
 def validate_opcos(df,active_opco_list,column):
+    """
+        validate opco id
+    """
     invalid_df = df.filter(~df[column].isin(active_opco_list) | df[column].isNull())
     if len(invalid_df.head(1)) > 0:
         invalid_df.show(truncate=False)
@@ -8,6 +16,9 @@ def validate_opcos(df,active_opco_list,column):
     return get_opco_list(invalid_df)
 
 def validate_column(df, column):
+    """
+        validate column
+    """
     invalid_df = df.filter((df[column] == "") | df[column].isNull() | (df[column].rlike('[^0-9]')) | isnan(df[column]))
     if len(invalid_df.head(1)) > 0:
         invalid_df.show(truncate=False)
@@ -15,13 +26,22 @@ def validate_column(df, column):
     return get_opco_list(invalid_df)
 
 def get_opco_list(df):
+    """
+        get opco list
+    """
     return [row.opco_id for row in df.select('opco_id').distinct().collect()]
 
 def remove_records_of_given_opcos(df, failed_opco_list):
+    """
+        remove records of error opco
+    """
     return df.filter(~df.opco_id.isin(failed_opco_list))
 
 
 def validate_column_length_less_than(df, column, col_length):
+    """
+        validate column length
+    """
     invalid_df = df.filter((length(df[column]) > col_length))
     if len(invalid_df.head(1)) > 0:
         invalid_df.show(truncate=False)
@@ -30,6 +50,9 @@ def validate_column_length_less_than(df, column, col_length):
 
 
 def validate_column_length_equals(df, column, col_length):
+    """
+        validate column length
+    """
     invalid_df = df.filter((length(df[column]) != col_length))
     if len(invalid_df.head(1)) > 0:
         invalid_df.show(truncate=False)
@@ -37,6 +60,9 @@ def validate_column_length_equals(df, column, col_length):
 
 
 def validate_data_range(df, column, min_val, max_val):
+    """
+        validate data range
+    """
     invalid_df = df.filter((df[column] < min_val) | (df[column] > max_val))
     if len(invalid_df.head(1)) > 0:
         invalid_df.show(truncate=False)
@@ -47,6 +73,9 @@ def validate_data_range(df, column, min_val, max_val):
 
 
 def validate_date_format(df, column, input_date_regex, input_date_format):
+    """
+        validate date format
+    """
     invalid_df = df.filter(df[column].isNull() | ~(df[column].rlike(input_date_regex)))
     if len(invalid_df.head(1)) > 0:
         invalid_df.show(truncate=False)
@@ -57,6 +86,9 @@ def validate_date_format(df, column, input_date_regex, input_date_format):
 
 
 def validate_date_time_field(df, output_column):
+    """
+        validate_date_time_field
+    """
     invalid_df = df.filter(df[output_column].isNull())
     if len(invalid_df.head(1)) > 0:
         invalid_df.show(truncate=False)
