db4e71911c12a51efcfafef1ac54f34848f21d27
sithija <sithija.thewahettige@sysco.com>
2021-06-04 10:35:15 +0530

    [TRSP-79] pylint updates

--- src/price_zone/data_backup_job.py
+++ src/price_zone/data_backup_job.py
@@ -8,8 +8,7 @@ from datetime import datetime
 from urllib.parse import urlparse
 
 from awsglue.utils import getResolvedOptions
-from move_s3_objects import copy_input_file, copy_objects_with_prefix, \
-    delete_object, delete_directory
+from move_s3_objects import copy_input_file, copy_objects_with_prefix, delete_object, delete_directory
 
 if __name__ == "__main__":
     print("started Price Zone data moving for archival and cleaning\n")
@@ -42,53 +41,31 @@ if __name__ == "__main__":
 
     archiving_path = backup_file_path
 
-    decompressed_file_parsed_path = urlparse(
-        decompressed_file_path, allow_fragments=False)
+    decompressed_file_parsed_path = urlparse(decompressed_file_path, allow_fragments=False)
     decompressed_file_bucket = decompressed_file_parsed_path.netloc
-    decompressed_file_key = decompressed_file_parsed_path.path.lstrip('/')
+    decompressed_file = decompressed_file_parsed_path.path.lstrip('/')
 
     input_file_destination_key = archiving_path + s3_input_file_key
-    decompressed_file_destination_key = archiving_path + \
-                                        decompressed_file_key.split('/')[-1]
-    copy_input_file(
-        s3_input_bucket,
-        s3_input_file_key,
-        archiving_s3_bucket,
-        input_file_destination_key)
+    decompressed_file_path_key = archiving_path + decompressed_file.split('/')[-1]
+
+    copy_input_file(s3_input_bucket, s3_input_file_key, archiving_s3_bucket, input_file_destination_key)
 
     # do this if gzip only
     if file_type == 'gz':
-        copy_input_file(
-            decompressed_file_bucket,
-            decompressed_file_key,
-            archiving_s3_bucket,
-            decompressed_file_destination_key)
+        copy_input_file(decompressed_file_bucket, decompressed_file, archiving_s3_bucket, decompressed_file_path_key)
 
     METADATA_FILE = '{}/additionalInfo.txt'.format(intermediate_directory_path)
-    copy_input_file(
-        decompressed_file_bucket,
-        METADATA_FILE,
-        archiving_s3_bucket,
-        archiving_path +
-        'additionalInfo.txt')
+    copy_input_file(decompressed_file_bucket, METADATA_FILE, archiving_s3_bucket, archiving_path + 'additionalInfo.txt')
 
     opco_partitioned_path = archiving_path + 'partitioned/'
-    copy_objects_with_prefix(
-        intermediate_s3_bucket,
-        partitioned_files_path,
-        archiving_s3_bucket,
-        opco_partitioned_path)
+    copy_objects_with_prefix(intermediate_s3_bucket, partitioned_files_path, archiving_s3_bucket, opco_partitioned_path)
 
     # cleaning source files
     delete_object(s3_input_bucket, s3_input_file_key)
 
     # do this if gzip only
     if file_type == 'gz':
-        delete_object(decompressed_file_bucket, decompressed_file_key)
+        delete_object(decompressed_file_bucket, decompressed_file)
 
-    delete_directory(
-        source_bucket=intermediate_s3_bucket,
-        prefix=partitioned_files_path)
-    delete_directory(
-        source_bucket=intermediate_s3_bucket,
-        prefix=intermediate_directory_path)
+    delete_directory(source_bucket=intermediate_s3_bucket, prefix=partitioned_files_path)
+    delete_directory(source_bucket=intermediate_s3_bucket, prefix=intermediate_directory_path)
